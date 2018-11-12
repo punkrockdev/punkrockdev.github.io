709d2c954d8fd8f0c4d07d820f447a2da01ec044
Dražen Lučanin <kermit666@gmail.com>
2018-11-12 23:18:23 +0100

    add webp versions of images

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const image = require('gulp-image');
+const webp = require('gulp-webp');
 
+
+// just resize the images a bit
 gulp.task('image', function () {
   gulp.src('./images/*')
     .pipe(image())
     .pipe(gulp.dest('./images'));
 });
 
-gulp.task('default', ['image']);
+// create .webp version of the images
+gulp.task('image-webp', function () {
+  gulp.src('./images/**/*')
+    .pipe(webp())
+    .pipe(gulp.dest('./images/'));
+});
+
+gulp.task('default', ['image', 'image-webp']);
