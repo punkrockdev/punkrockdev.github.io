96789300fdcff8317678c0f95b62354e0485a412
Dražen Lučanin <kermit666@gmail.com>
2018-11-17 19:52:25 +0100

    tweak images a bit more

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 const gulp = require('gulp');
-const image = require('gulp-image');
+// const image = require('gulp-image');
 const webp = require('gulp-webp');
 
 
-// just resize the images a bit
-gulp.task('image', function () {
-  gulp.src('./images/*')
-    .pipe(image())
-    .pipe(gulp.dest('./images'));
-});
+// // just resize the images a bit
+// gulp.task('image', function () {
+//   gulp.src('./images/*')
+//     .pipe(image())
+//     .pipe(gulp.dest('./images'));
+// });
 
 // create .webp version of the images
 gulp.task('image-webp', function () {
-  gulp.src('./images/**/*')
-    .pipe(webp())
+  gulp.src('./images/**/*.{jpg,png,tif}')
+    .pipe(webp({preset: 'photo', quality: 90, method: 6, lossless: true}))
     .pipe(gulp.dest('./images/'));
 });
 
-gulp.task('default', ['image', 'image-webp']);
+gulp.task('default', ['image-webp']);
