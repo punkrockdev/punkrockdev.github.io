904cd177bb7c70b6cd71b48d975d49d2b217e1a8
Antonija Simic <antonija1023@gmail.com>
2019-03-25 12:53:35 +0100

    hide video on mobile; move Js for video to its own file

---
+++ js/video.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function($) {
+  loadVideo();
+
+  $(window).resize(function() {
+    loadVideo();
+  });
+
+  function loadVideo() {
+    let srcMp4 =
+      "https://static.punkrockdev.com/file/punkrockdev/Edit+v5+HQ.mp4";
+    let breakpoint = 1200;
+
+    var video = document.getElementById("header-video");
+    // Remove existing source tags for mobile
+    if ($(document).width() < breakpoint + 1) {
+      while (video.firstChild) {
+        video.removeChild(video.firstChild);
+      }
+    }
+
+    // Add source tags if not already present
+    if ($(document).width() > breakpoint) {
+
+      if (document.querySelectorAll("#header-video > source").length < 1) {
+        console.log(this);
+        var source1 = document.createElement("source");
+        source1.setAttribute("src", srcMp4);
+        source1.setAttribute("type", "video/mp4");
+        video.appendChild(source1);
+      }
+
+      // Play the video
+      $("#header-video")[0].play();
+    }
+  }
+});
